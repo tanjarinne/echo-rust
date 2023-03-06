69f8e571c7165a0e17b1c69ca2e9c8bfc0db3740
Tanja Alva <tanja.alva@gmail.com>
2023-03-06 16:36:46 +0800

    It's working, but without the headers and without logging and all the other nice things

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn handle_request(stream: &mut TcpStream) {
+    let mut request = String::new();
+    let mut buf: [u8; 1024] = [0 as u8; 1024];
 
+    match stream.read(&mut buf) {
+        Ok(size) => request = String::from_utf8_lossy(&buf[0..size]).to_string(),
+        Err(e) => print!("Error: {}", e),
+    }
+
+    let response = format!("HTTP/1.1 200 OK\r\n\
+        Server: Echo\r\n\
+        Content-Type: text/plain\r\n\
+        Content-Length: {}\r\n\r\n\
+        {}", request.len(), request);
+
+    match stream.write(response.as_bytes()) {
+        Ok(_) => println!("algo"),
+        Err(e) => println!("Error: {}", e),
+    }
 }
 
 fn main() {
     let host = std::env::var("ECHO_HOST").unwrap_or("0.0.0.0".to_string());
     let port = std::env::var("ECHO_PORT").unwrap_or("8989".to_string());
-    let bind_addr = format!("{host}:{port}");
-    let listener = TcpListener::bind(bind_addr).unwrap();
+    let listener = TcpListener::bind(format!("{host}:{port}")).unwrap();
 
     listener.incoming().for_each(|stream| {
         match stream {
