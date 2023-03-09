df1f9d843484788b5468f388b05069904899d97a
Tanja Alva <tanja.alva@gmail.com>
2023-03-09 13:30:24 +0800

    Almost working - this solution is memory efficient, but sometimes hangs up

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,47 @@
-use std::io::{Read, Write};
+use std::io::{Read, Write, BufReader, BufRead};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_request(stream: &mut TcpStream) {
-    let mut request = String::new();
-    let mut buf: [u8; 1024] = [0 as u8; 1024];
 
-    match stream.read(&mut buf) {
-        Ok(size) => request = String::from_utf8_lossy(&buf[0..size]).to_string(),
-        Err(e) => print!("Error: {}", e),
+fn parse_request_line(line: &str) -> (&str, &str, &str) {
+    let mut parts = line.trim().split_whitespace();
+    let method = parts.next().unwrap();
+    let path = parts.next().unwrap();
+    let version = parts.next().unwrap();
+
+    (method, path, version)
+}
+
+fn handle_request(stream: &mut TcpStream) {
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut line = String::new();
+
+    reader.read_line(&mut line).unwrap();
+    let (method, path, _version) = parse_request_line(&line);
+    println!("{} {}", &method, &path);
+    line.clear();
+
+    let mut headers = vec![];
+    while reader.read_line(&mut line).unwrap() > 0 {
+        if line == "\r\n" {
+            break;
+        }
+        headers.push(line.clone());
+        line.clear();
     }
 
+    let mut body = String::new();
+    reader.read_to_string(&mut body).unwrap();
+    let response_body = format!("{}\r\n{}", &headers.join("\n"), &body);
     let response = format!("HTTP/1.1 200 OK\r\n\
         Server: Echo\r\n\
         Content-Type: text/plain\r\n\
         Content-Length: {}\r\n\r\n\
-        {}", request.len(), request);
+        {response_body}", response_body.len());
 
-    match stream.write(response.as_bytes()) {
-        Ok(_) => println!("algo"),
+    match stream.write_all(response.as_bytes()) {
+        Ok(_) => {
+            stream.flush().unwrap();
+        }
         Err(e) => println!("Error: {}", e),
     }
 }
